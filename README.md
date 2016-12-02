ca24508cd8521f3f145d2a80d1880893d9f9e6f6
Alex-xd <i@alexxd.com>
2016-12-02 10:36:28 +0800

    update  bug fixde

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# my-ghost-theme
-我的ghost博客主题
+## Ano
+
+Ano，一个简约适用的ghost主题，基于[onevcat/vno](https://github.com/onevcat/vno)进行的二次开发。
+
+也是我的ghost博客主题 
+
+Demo  [https://i.alexxd.com](https://i.alexxd.com)
+
+预览
+
+![preview](http://o6x2vif88.bkt.clouddn.com/blogshow.png)
+
+
+## 开发
+
+### ghost根目录pm2启动或npm启动
+
+```bash
+sudo NODE_ENV=development pm2 start index.js --name "ghost"
+sudo pm2 save & sudo pm2 startup
+```
+
+or
+
+`npm start`
+
+
+### sass
+
+css依赖`bounce`，安装方式见官网[http://bourbon.io/](http://bourbon.io/)
+
+监听
+
+`sass --watch assets/scss/vno.scss:assets/css/vno.css --style compressed`
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,36 @@
 $(document).ready(function () {
     var panelCover = $('.panel-cover'),
-        navWrapper = $('.navigation-wrapper');
+        navWrapper = $('.navigation-wrapper'),
+        mainPostList = $('.main-post-list'),
+        wrapper = $('.content-wrapper');
 
-    $('a.blog-button').click(function () {
-        // If already in blog, return early without animate overlay panel again.
-        if (location.hash && location.hash == "#blog") return;
-        if (panelCover.hasClass('panel-cover--collapsed')) return;
-        $('.main-post-list').removeClass('hidden');
-        currentWidth = panelCover.width();
-        if (currentWidth < 960) {
-            panelCover.addClass('panel-cover--collapsed');
-            $('.content-wrapper').addClass('animated slideInRight');
-        } else {
-            panelCover.css('max-width', currentWidth);
-            panelCover.animate({ 'max-width': '700px', 'width': '28%' }, 400, swing = 'swing', function () {});
-        }
-    });
-
-    $('a.blog-button.homepage').click(function(){
-         // If already in blog, return early without animate overlay panel again.
-        if (location.hash && location.hash != "#blog") return;
-        if (!panelCover.hasClass('panel-cover--collapsed')) return;
-        $('.main-post-list').addClass('hidden');
-        currentWidth = panelCover.width();
-        if (currentWidth < 960) {
-            panelCover.removeClass('panel-cover--collapsed');
-            $('.content-wrapper').removeClass('animated slideInRight');
-        } else {
-            panelCover.css('max-width', currentWidth);
-            panelCover.animate({ 'max-width': 'none', 'width': '100%' }, 400, swing = 'swing', function () {});
-        }
-    });
-
-    if (window.location.hash && window.location.hash == "#blog") {
-        panelCover.addClass('panel-cover--collapsed');
-        $('.main-post-list').removeClass('hidden');
+    if (window.location.hash === '#blog') {
+        toBlog();
     }
+    if (window.location.hash === '#') {
+        toHome();
+    }
+
+    function toHome() {
+        if (window.location.hash !== "#blog") {
+            return;
+        }
+        panelCover.removeClass('panel-cover--collapsed');
+        wrapper.addClass('hidden');
+        wrapper.removeClass('animated slideInRight');
+    }
+
+    function toBlog() {
+        if (window.location.hash === "#blog") {
+            return;
+        }
+        panelCover.addClass('panel-cover--collapsed');
+        wrapper.removeClass('hidden');
+        wrapper.addClass('animated slideInRight');
+    }
+
+    $('a.blog-button').click(toBlog);
+    $('a.blog-button.homepage').click(toHome);
 
     if (window.location.pathname.substring(0, 5) == "/tag/") {
         panelCover.addClass('panel-cover--collapsed');
@@ -55,13 +50,15 @@ $(document).ready(function () {
     });
 
     $('.navigation-wrapper .blog-button').click(function () {
-        if (navWrapper.css('display') == "block") {
-            navWrapper.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-                navWrapper.toggleClass('visible animated bounceOutUp');
-            });
+        // if (navWrapper.css('display') == "block") {
+        //     navWrapper.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        //         navWrapper.toggleClass('visible animated bounceOutUp');
+        //     });
 
-            navWrapper.toggleClass('animated bounceInDown animated bounceOutUp');
-        }
+        //     navWrapper.toggleClass('animated bounceInDown ','animated bounceOutUp');
+        // }
+        // navWrapper.toggleClass('animated bounceInDown ','animated bounceOutUp');
+
 
         $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
     });
